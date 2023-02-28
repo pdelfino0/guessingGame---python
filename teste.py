17b5dc7a06e4265d78b9a62c37686bc551e4a3ce
Pedro Delfino <pedrofernandesdelfino@gmail.com>
2023-02-28 12:44:26 -0300

    Translated code

--- teste.py
+++ teste.py
@@ -1,22 +1,21 @@
 import random
 
-sorteado = random.randint(1, 101)
+drawn = random.randint(1, 100)
 
-acertou = False
+hit = False
 
 for tent in range(1, 11):
-        print(f"{sorteado}")
-        print(f"Tentativa {tent}:", end=" ")
-        chute = int(input("Digite o chute: "))
+        print(f"Attempt {tent}:", end=" ")
+        shot = int(input("Give your shot: "))
 
-        if chute > sorteado:
-            print("Tente um número mais baixo!")
-        elif chute < sorteado:
-            print("Tente um número mais alto")
+        if shot > drawn:
+            print("Guess a lower number!")
+        elif shot < drawn:
+            print("Guess a higher number!")
         else:
-           acertou = True
+           hit = True
            break
-if acertou:
-    print("Parabéns, você acertou!")
+if hit:
+    print("Congrats, you got it right!")
 else:
-    print(f"Que pena, suas tentativas acabaram! O número era: {sorteado}")
+    print(f"What a pitty, your attempts are over! The number was : {drawn}")
